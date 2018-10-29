dd69ef5c2fac9b97674d2c69a9654efc5f539704
Abi Raskin <abiraskin@gmail.com>
2018-10-29 01:57:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word("Hello there, and how are you?")
-  reverse_each_word.spilt
-  reverse_each_word.collect do |words|
-
-  reverse_each_word.reverse
+def reverse_each_word(sentence)
+  sentense.spilt.collect do |word|
+    word.reverse
+  end
+  sentece.join(" ")
 end
 end
